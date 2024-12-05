79f697146d07b6972150bab8680ebf0c265692ba
DittoxR <ronald.rosarioh@gmail.com>
2024-12-05 02:47:57 +0000

    ayuda

--- src/models/db.js
+++ src/models/db.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 // Conexión a la base de datos MySQL en un contenedor
-const sequelize = new Sequelize('noteblog', 'usuario', 'contraseña', {
-    host: 'mysql-container',  // Nombre del contenedor MySQL
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,  // Usar la variable de entorno para el host
+    port: process.env.DB_PORT,  // Usar la variable de entorno para el puerto
     dialect: 'mysql',
 });
 
