8cc9aae457b5446c82672aae97e7c10880c6de7e
DittoxR <ronald.rosarioh@gmail.com>
2024-12-05 02:17:38 +0000

    OJala

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,11 @@
+require('dotenv').config(); // Cargar las variables de entorno
+
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('noteblog', 'usuario', 'contraseña', {
-    host: 'localhost',
+
+// Configurar Sequelize con las variables de entorno
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // Asegúrate de que la variable DB_PORT esté configurada en tu .env
     dialect: 'mysql',
 });
 
